2ed3c5914a46347567677b3a72f872b9591fccfb
Georgios Balasis <gbalasis@evolutiongaming.com>
2017-07-25 13:58:02 +0300

    Add alpha detection (#1)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About
 
-This is a tool that generates PVRTC, ETC1, ETC2, ASTC textures from png and jpg files. It can run on node or as a gulp task.
+This is a tool that generates PVRTC, ETC1, ETC2, ASTC, DXT textures from png and jpg files. It can run on node or as a gulp task.
 
 It is meant to work with [Babylon.js](https://github.com/BabylonJS/Babylon.js). The tool is a port of two [.bat files](https://github.com/BabylonJS/Babylon.js/tree/master/Tools/CompressedTextured).
 
@@ -22,7 +22,7 @@ Import/require the exposed function `generateTextures` to your script. It accept
 * `PVRTexToolCLI` - *string*: The absolute path to the PVRTexToolCLI tool.
 * `inputDir` - *string*: The directory where the image files are located. It will be read recursively and all the images will be used in order to generate the new textures.
 * `quality` - *string (optional), (options "low"/"high", default: "high")*: The quality of the exported textures.
-* `exportFormats` - *string[] (optional) (options "PVRTC"/"ETC1"/"ETC2"/"ASTC", default: ["PVRTC", "ETC1", "ETC2", "ASTC"])*: The format of the exported textures.
+* `exportFormats` - *string[] (optional) (options "PVRTC"/"ETC1"/"ETC2"/"ASTC"/"DXT", default: ["PVRTC", "ETC1", "ETC2", "ASTC", "DXT"])*: The format of the exported textures.
 
 Example usage:
 ```javascript
@@ -34,6 +34,10 @@ createGpuTexture({
     PVRTexToolCLI: "/Applications/Imagination/PowerVR_Graphics/PowerVR_Tools/PVRTexTool/CLI/OSX_x86/PVRTexToolCLI",
     inputDir: "/src",
     quality: "high",
-    exportFormats: ["PVRTC", "ETC1", "ETC2", "ASTC"]
+    exportFormats: ["PVRTC", "ETC1", "ETC2", "ASTC", "DXT"]
 });
 ```
+
+### Limitations
+
+DXT files can not be exported on MacOS currently.
--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 import { join } from "path";
-import { statSync, readdirSync } from "fs";
+import { stat, statSync, readdir, readdirSync, open, openSync, read, readSync, close, closeSync, Stats } from "fs";
 import * as shelljs from "shelljs";
 
 export enum TextureType {
@@ -20,6 +20,7 @@ export interface TextureGeneratorProps {
     inputDir: string;
     quality: TextureQuality;
     exportFormats: TextureType[];
+    async: boolean;
 }
 
 interface CliConverterProps {
@@ -27,6 +28,7 @@ interface CliConverterProps {
     file: string;
     quality?: string;
     hasAlpha?: boolean;
+    async: boolean;
 }
 
 type ImageConverterProps = CliConverterProps & { exportFormats: string[] };
@@ -40,6 +42,7 @@ export default function generateTextures(
         inputDir,
         quality = TextureQuality.HIGH,
         exportFormats = [TextureType.PVRTC, TextureType.ETC1, TextureType.ETC2, TextureType.ASTC, TextureType.DXT],
+        async = false,
     }: TextureGeneratorProps) {
     // Mac does not support DXT format
     const indexOfDXT = exportFormats.indexOf(TextureType.DXT);
@@ -47,81 +50,160 @@ export default function generateTextures(
         console.warn("DXT format is not supported on MacOS");
         exportFormats.splice(indexOfDXT, 1);
     }
-    readImages({ PVRTexToolCLI, inputDir, quality, exportFormats });
+    readImages({ PVRTexToolCLI, inputDir, quality, exportFormats, async });
 }
 
-function readImages({ PVRTexToolCLI, inputDir, quality, exportFormats }: TextureGeneratorProps) {
-    readdirSync(inputDir).forEach((file: string) => {
-        const filePath = join(inputDir, file);
-        if (statSync(filePath).isDirectory()) {
-            readImages({ PVRTexToolCLI, inputDir: filePath, quality, exportFormats });
+function readImages({ PVRTexToolCLI, inputDir, quality, exportFormats, async }: TextureGeneratorProps) {
+    const openFileOrDir = (stats: Stats, filePath: string, fileName: string) => {
+        if (stats.isDirectory()) {
+            readImages({ PVRTexToolCLI, inputDir: filePath, quality, exportFormats, async });
         } else {
-            const extension = file.substr(file.lastIndexOf(".") + 1).toLowerCase();
+            const extension = fileName.substr(fileName.lastIndexOf(".") + 1).toLowerCase();
             if (["jpg", "jpeg"].indexOf(extension) >= 0) {
-                convertImage({ PVRTexToolCLI, file: filePath, quality, hasAlpha: false, exportFormats });
-            } else if ("png" === extension) {
-                convertImage({ PVRTexToolCLI, file: filePath, quality, hasAlpha: true, exportFormats });
+                convertImage({ PVRTexToolCLI, file: filePath, quality, hasAlpha: false, exportFormats, async });
+            } else if (extension === "png") {
+                if (async) {
+                    hasAlphaAsync(filePath, (hasAlpha: boolean) => {
+                        convertImage({
+                            PVRTexToolCLI,
+                            file: filePath,
+                            quality,
+                            hasAlpha,
+                            exportFormats,
+                            async,
+                        });
+                    });
+                } else {
+                    convertImage({
+                        PVRTexToolCLI,
+                        file: filePath,
+                        quality,
+                        hasAlpha: hasAlphaSync(filePath),
+                        exportFormats,
+                        async,
+                    });
+                }
             }
         }
+    };
+    if (async) {
+        readdir(inputDir, (err: NodeJS.ErrnoException, files: string[]) => {
+            if (err) {
+                throw err;
+            }
+            files.forEach((fileName: string) => {
+                const filePath = join(inputDir, fileName);
+                stat(filePath, (statErr: NodeJS.ErrnoException, stats: Stats) => {
+                    if (statErr) {
+                        throw statErr;
+                    }
+                    openFileOrDir(stats, filePath, fileName);
+                });
+            });
+        });
+    } else {
+        readdirSync(inputDir).forEach((fileName: string) => {
+            const filePath = join(inputDir, fileName);
+            openFileOrDir(statSync(filePath), filePath, fileName);
+        });
+    }
+}
+
+function hasAlphaAsync(file: string, callback: (hasAlpha: boolean) => void) {
+    const buffer = new Buffer(1);
+    open(file, "r", (err: NodeJS.ErrnoException, fd: number) => {
+        if (err) {
+            throw err;
+        }
+        read(fd, buffer, 0, 1, 25, (readErr: NodeJS.ErrnoException, bytesRead: number, buf: Buffer) => {
+            if (readErr) {
+                throw readErr;
+            }
+            callback(6 === buf[0]);
+            close(fd);
+        });
     });
 }
 
-function convertImage({ PVRTexToolCLI, file, quality, hasAlpha, exportFormats }: ImageConverterProps) {
+function hasAlphaSync(file: string): boolean {
+    const buffer = new Buffer(1);
+    const fd: number = openSync(file, "r");
+    readSync(fd, buffer, 0, 1, 25);
+    closeSync(fd);
+    return 6 === buffer[0];
+}
+
+function convertImage({ PVRTexToolCLI, file, quality, hasAlpha, exportFormats, async }: ImageConverterProps) {
     if (exportFormats.indexOf(TextureType.PVRTC) >= 0) {
-        convertToPVRTC({ PVRTexToolCLI, file, quality, hasAlpha });
+        convertToPVRTC({ PVRTexToolCLI, file, quality, hasAlpha, async });
     }
     if (exportFormats.indexOf(TextureType.ETC1) >= 0) {
-        convertToETC1({ PVRTexToolCLI, file, quality, hasAlpha });
+        convertToETC1({ PVRTexToolCLI, file, quality, hasAlpha, async });
     }
     if (exportFormats.indexOf(TextureType.ETC2) >= 0) {
-        convertToETC2({ PVRTexToolCLI, file, quality, hasAlpha });
+        convertToETC2({ PVRTexToolCLI, file, quality, hasAlpha, async });
     }
     if (exportFormats.indexOf(TextureType.ASTC) >= 0) {
-        convertToASTC({ PVRTexToolCLI, file, quality });
+        convertToASTC({ PVRTexToolCLI, file, quality, async });
     }
     if (exportFormats.indexOf(TextureType.DXT) >= 0) {
-        convertToDXT({ PVRTexToolCLI, file, hasAlpha });
+        convertToDXT({ PVRTexToolCLI, file, hasAlpha, async });
     }
 }
 
-function convertToPVRTC({ PVRTexToolCLI, file, quality, hasAlpha }: CliConverterProps) {
+function convertToPVRTC({ PVRTexToolCLI, file, quality, hasAlpha, async }: CliConverterProps) {
     const filename = file.substr(0, file.lastIndexOf("."));
     const format = hasAlpha ? "PVRTC1_2" : "PVRTC1_2_RGB";
     const fileQuality = quality === TextureQuality.HIGH ? "pvrtcbest" : "pvrtcfastest";
     // tslint:disable-next-line:max-line-length
-    shelljs.exec(`${PVRTexToolCLI} -i "${file}" -flip y -pot + -square + -m -dither -f ${format},UBN,lRGB -q ${fileQuality} -o "${filename}-pvrtc.ktx"`);
+    shelljs.exec(
+        `${PVRTexToolCLI} -i "${file}" -flip y -pot + -square + -m -dither -f ${format},UBN,lRGB -q ${fileQuality} -o "${filename}-pvrtc.ktx"`,
+        { async },
+    );
 }
 
-function convertToETC1({ PVRTexToolCLI, file, quality, hasAlpha }: CliConverterProps) {
+function convertToETC1({ PVRTexToolCLI, file, quality, hasAlpha, async }: CliConverterProps) {
     if (hasAlpha) {
         return;
     }
     const filename = file.substr(0, file.lastIndexOf("."));
     const fileQuality = quality === TextureQuality.HIGH ? "etcslowperceptual" : "etcfast";
     // tslint:disable-next-line:max-line-length
-    shelljs.exec(`${PVRTexToolCLI} -i "${file}" -flip y -pot + -m -f ETC1,UBN,lRGB -q ${fileQuality} -o "${filename}-etc1.ktx"`);
+    shelljs.exec(
+        `${PVRTexToolCLI} -i "${file}" -flip y -pot + -m -f ETC1,UBN,lRGB -q ${fileQuality} -o "${filename}-etc1.ktx"`,
+        { async },
+    );
 }
 
-function convertToETC2({ PVRTexToolCLI, file, quality, hasAlpha }: CliConverterProps) {
+function convertToETC2({ PVRTexToolCLI, file, quality, hasAlpha, async }: CliConverterProps) {
     const filename = file.substr(0, file.lastIndexOf("."));
     const format = hasAlpha ? "ETC2_RGBA" : "ETC2_RGB";
     const fileQuality = quality === TextureQuality.HIGH ? "etcslowperceptual" : "etcfast";
     // tslint:disable-next-line:max-line-length
-    shelljs.exec(`${PVRTexToolCLI} -i "${file}" -flip y -pot + -m -f ${format},UBN,lRGB -q ${fileQuality} -o "${filename}-etc2.ktx"`);
+    shelljs.exec(
+        `${PVRTexToolCLI} -i "${file}" -flip y -pot + -m -f ${format},UBN,lRGB -q ${fileQuality} -o "${filename}-etc2.ktx"`,
+        { async },
+    );
 }
 
-function convertToASTC({ PVRTexToolCLI, file, quality }: CliConverterProps) {
+function convertToASTC({ PVRTexToolCLI, file, quality, async }: CliConverterProps) {
     const filename = file.substr(0, file.lastIndexOf("."));
     const fileQuality = quality === TextureQuality.HIGH ? "astcexhaustive" : "astcveryfast";
     // tslint:disable-next-line:max-line-length
-    shelljs.exec(`${PVRTexToolCLI} -i "${file}" -flip y -pot + -m -f ASTC_8x8,UBN,lRGB -q ${fileQuality} -o "${filename}-astc.ktx"`);
+    shelljs.exec(
+        `${PVRTexToolCLI} -i "${file}" -flip y -pot + -m -f ASTC_8x8,UBN,lRGB -q ${fileQuality} -o "${filename}-astc.ktx"`,
+        { async },
+    );
 }
 
-function convertToDXT({ PVRTexToolCLI, file, hasAlpha }: CliConverterProps) {
+function convertToDXT({ PVRTexToolCLI, file, hasAlpha, async }: CliConverterProps) {
     const filename = file.substr(0, file.lastIndexOf("."));
     const format = hasAlpha ? "BC2" : "BC1";
     // tslint:disable-next-line:max-line-length
-    shelljs.exec(`${PVRTexToolCLI} -i "${file}" -flip y -pot + -m -f ${format},UBN,lRGB -o "${filename}-dxt.ktx"`);
+    shelljs.exec(
+        `${PVRTexToolCLI} -i "${file}" -flip y -pot + -m -f ${format},UBN,lRGB -o "${filename}-dxt.ktx"`,
+        { async },
+    );
 }
 
 if (module && module.hasOwnProperty("exports")) {
